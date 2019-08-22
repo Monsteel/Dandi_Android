771c7cae56f94c1fa2f99944648d17182a605bd1
kjy031104@gmail.com <kjy031104@gmail.com>
2019-08-22 22:23:09 +0900

    login 2

---
+++ app/src/main/java/org/techtown/schooler/Activity/Login.java
@@ -0,0 +1,59 @@
+package org.techtown.schooler.Activity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.techtown.schooler.MainActivity;
+import org.techtown.schooler.R;
+import org.techtown.schooler.network.Data;
+import org.techtown.schooler.network.LoginPostRequest;
+import org.techtown.schooler.network.NetRetrofit;
+import org.techtown.schooler.network.response.Response;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
+public class Login {
+
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            String id = "대소고 존잘남:이영은";
+            String pw = "없다";
+            login(new LoginPostRequest(id, pw));
+
+        }
+
+        private void login(LoginPostRequest loginPostRequest) {
+            final Call<Response<Data>> res = NetRetrofit.getInstance().getService().loginPost(loginPostRequest);
+            res.enqueue(new Callback<Response<Data>>() {
+                @Override
+                public void onResponse(Call<Response<Data>> call, retrofit2.Response<Response<Data>> response) {
+
+                    Integer Status = response.code();
+                    String Message = response.message();
+                    try {
+                        if (Status == 200 || Status == 401 || Status == 403 || Status == 500) {
+                            Toast.makeText(org.techtown.schooler.MainActivity.this, Status + ":" + Message, Toast.LENGTH_SHORT).show();
+                            Log.d("[Login] Status", Status + ":" + Message);
+                        }
+                    }  catch (Exception err) {
+                        //Toast.makeText(MainActivity.this, "네트워크 연결 오류", Toast.LENGTH_SHORT).show();
+                        Log.e("[Login][ERROR] : ", "네트워크 연결 오류");
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<org.techtown.schooler.network.response.Response<Data>> call, Throwable t) {
+                    Log.e("Err", t.getMessage());
+                }
+            });
+        }
+    }
+}
