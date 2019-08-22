99dfadbb0fca7890e114c3f741c06ec1b77ca564
Monsteel <eun36739@gmail.com>
2019-08-22 22:24:21 +0900

    MainEdit

--- app/src/main/java/org/techtown/schooler/MainActivity.java
+++ app/src/main/java/org/techtown/schooler/MainActivity.java
@@ -3,6 +3,7 @@ package org.techtown.schooler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.icu.text.IDNA;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,35 +27,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String id = "대소고 존잘남:이영은";
-        String pw = "없다";
-        login(new LoginPostRequest(id, pw));
 
-    }
-
-    private void login(LoginPostRequest loginPostRequest) {
-        final Call<Response<Data>> res = NetRetrofit.getInstance().getService().loginPost(loginPostRequest);
-        res.enqueue(new Callback<Response<Data>>() {
-            @Override
-            public void onResponse(Call<org.techtown.schooler.network.response.Response<Data>> call, retrofit2.Response<Response<Data>> response) {
-
-                Integer Status = response.code();
-                String Message = response.message();
-                try {
-                    if (Status == 200 || Status == 401 || Status == 403 || Status == 500) {
-                        Toast.makeText(MainActivity.this, Status + ":" + Message, Toast.LENGTH_SHORT).show();
-                        Log.d("[Login] Status", Status + ":" + Message);
-                    }
-                }  catch (Exception err) {
-                    //Toast.makeText(MainActivity.this, "네트워크 연결 오류", Toast.LENGTH_SHORT).show();
-                    Log.e("[Login][ERROR] : ", "네트워크 연결 오류");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<org.techtown.schooler.network.response.Response<Data>> call, Throwable t) {
-                Log.e("Err", t.getMessage());
-            }
-        });
+        SharedPreferences LoginCheck = getSharedPreferences("Check", MODE_PRIVATE);
+        boolean first = LoginCheck.getBoolean("Check", false);//첫 실행임
+        if(first==false){
+            Intent StatMember = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(StatMember);
+        }else{
+            Log.d("Is first Time?", "not first");
+        }
     }
 }
