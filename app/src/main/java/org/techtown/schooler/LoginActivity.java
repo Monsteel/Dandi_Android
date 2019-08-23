869e98dbf1f266910f39cade5d66f865e44ece22
kjy031104@gmail.com <kjy031104@gmail.com>
2019-08-23 13:29:07 +0900

    change

---
+++ app/src/main/java/org/techtown/schooler/LoginActivity.java
@@ -0,0 +1,62 @@
+package org.techtown.schooler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.techtown.schooler.network.Data;
+import org.techtown.schooler.network.LoginPostRequest;
+import org.techtown.schooler.network.NetRetrofit;
+import org.techtown.schooler.network.response.Response;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // String 형을 사용해서 id, pw 변수에 데이터를 저장합니다.
+        String id = "uesr";
+        String pw = "user";
+
+        // login 매서드를 호출하면서 login 매서드의 파라미터인 LoginPostRequest 클래스로 위에서 생성한 id, pw 를 전달하고있습니다.
+        login(new LoginPostRequest(id, pw));
+
+    }
+
+    // login 매서드
+    private void login(LoginPostRequest loginPostRequest) {
+
+
+        final Call<Response<Data>> res = NetRetrofit.getInstance().getService().loginPost(loginPostRequest);
+        res.enqueue(new Callback<Response<Data>>() {
+            @Override
+            public void onResponse(Call<Response<Data>> call, retrofit2.Response<Response<Data>> response) {
+
+                Integer Status = response.body().getStatus();
+                String Message = response.body().getMessage();
+                try {
+                    if (Status == 200 || Status == 401 || Status == 403 || Status == 500) {
+                        Toast.makeText(LoginActivity.this, Status + ":" + Message, Toast.LENGTH_SHORT).show();
+                        Log.d("[Login] Status", Status + ":" + Message);
+                    }
+                } catch (Exception err) {
+                    //Toast.makeText(MainActivity.this, "네트워크 연결 오류", Toast.LENGTH_SHORT).show();
+                    Log.e("[Login][ERROR] : ", "네트워크 연결 오류");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Response<Data>> call, Throwable t) {
+                Log.e("Err", t.getMessage());
+            }
+        });
+    }
+}
+
--- app/src/main/java/org/techtown/schooler/MainActivity.java
+++ app/src/main/java/org/techtown/schooler/MainActivity.java
@@ -6,8 +6,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-
-import org.techtown.schooler.Activity.Login.LoginActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,19 +22,29 @@ public class MainActivity extends AppCompatActivity {
 
         // boolean 자료형을 사용하여 first 변수를 생성 후 앞서 생성한 LoginCheck 변수에 있는 값을 getBoolean 속성을 사용합니다.
         // 파라미터로 위에서 생성한 getSharedPreferences 의 name 부분이 만약 null 값이라면 first 변수를 false 로 지정합니다.
-        boolean first = LoginCheck.getBoolean("Check", false);//첫 실행임
+        final boolean first = LoginCheck.getBoolean("Check", false);//첫 실행임
 
-        // first 변수가 false 일 경우
-        if(first==false){
 
-            // Intent 클래스를 사용해서 LoginActivity 화면으로 전환을 합니다.
-            Intent StatLogin = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(StatLogin);
-            Log.d("[LoginCheck]", "로그인 X");
+        Button button = findViewById(R.id.button);
 
-            // first 변수가 true 일 경우
-        }else{
-            Log.d("[LoginCheck] ", "로그인 O");
-        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // first 변수가 false 일 경우
+                if(first==false){
+
+                    // Intent 클래스를 사용해서 LoginActivity 화면으로 전환을 합니다.
+                    Intent StatLogin = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(StatLogin);
+                    Log.d("[LoginCheck]", "로그인 X");
+
+                    // first 변수가 true 일 경우
+                }else{
+                    Log.d("[LoginCheck] ", "로그인 O");
+                }
+            }
+        });
+
     }
 }
