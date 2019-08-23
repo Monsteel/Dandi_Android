ed6406585e4381353b0696ce3077bf425650f00f
Monsteel <eun36739@gmail.com>
2019-08-23 15:40:45 +0900

    login middle output

--- app/src/main/java/org/techtown/schooler/Login/LoginActivity.java
+++ app/src/main/java/org/techtown/schooler/Login/LoginActivity.java
@@ -25,7 +25,7 @@ public class LoginActivity extends AppCompatActivity{
         setContentView(R.layout.activity_login);
 
         // String 형을 사용해서 id, pw 변수에 데이터를 저장합니다.
-        String id = "user";
+        String id = "userr";
         String pw = "user";
 
 
@@ -45,22 +45,20 @@ public class LoginActivity extends AppCompatActivity{
     // login 매서드
     private void login(LoginPostRequest loginPostRequest) {
 
-
         final Call<Response<Data>> res = NetRetrofit.getInstance().getService().loginPost(loginPostRequest);
         res.enqueue(new Callback<Response<Data>>() {
             @Override
             public void onResponse(Call<Response<Data>> call, retrofit2.Response<Response<Data>> response) {
 
-                Integer Status = response.body().getStatus();
-                String Message = response.body().getMessage();
-                try {
-                    if (Status == 200 || Status == 401 || Status == 403 || Status == 500) {
-                        Toast.makeText(LoginActivity.this, Status + ":" + Message, Toast.LENGTH_SHORT).show();
-                        Log.d("[Login] Status", Status + ":" + Message);
-                    }
-                } catch (Exception err) {
-                    //Toast.makeText(MainActivity.this, "네트워크 연결 오류", Toast.LENGTH_SHORT).show();
-                    Log.e("[Login][ERROR] : ", "네트워크 연결 오류");
+                Integer Status = response.code();
+                if (Status == 200) {
+                    String Message =response.body().getMessage();
+                    Toast.makeText(LoginActivity.this, Status + ":" + Message, Toast.LENGTH_SHORT).show();
+                    Log.d("[Login] Status", Status + ":" + Message);
+
+                }else if (Status == 401 || Status == 403 || Status == 500){
+                    Toast.makeText(LoginActivity.this, Status+"", Toast.LENGTH_SHORT).show();
+                    Log.e("[Login][ERROR]", Status+"");
                 }
             }
 
--- app/src/main/java/org/techtown/schooler/network/Data.java
+++ app/src/main/java/org/techtown/schooler/network/Data.java
@@ -4,6 +4,15 @@ import org.techtown.schooler.Model.User;
 
 public class Data {
     private User user;
+    private Token token;
+
+    public Token getToken() {
+        return token;
+    }
+
+    public void setToken(Token token) {
+        this.token = token;
+    }
 
     public User getUser() {
         return user;
